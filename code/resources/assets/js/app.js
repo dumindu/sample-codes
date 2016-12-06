af4db35e0007a4ce271de8ea1d2fbe22450cbc9f
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2016-12-07 03:20:31 +0530

    Enabling x-editable on calendar

--- code/resources/assets/js/app.js
+++ code/resources/assets/js/app.js
@@ -4,5 +4,27 @@ require('bootstrap');
 require('../x-editable/js/bootstrap-editable');
 
 $( document ).ready(function() {
+    $('.calendar__day__room_count').editable({
+        type: 'number',
+        pk: function() {
+            var date = $(this).closest(".calendar__day").data('date');
+            var roomType = $(this).closest(".calendar__day__room_data").data('room-type');
+            return (date && roomType) ? {date: date, roomType: roomType} : '';
+        },
+        url: '/update',
+        title: 'Change Availability',
+        name: 'roomCount'
+    });
 
+    $('.calendar__day__room_price').editable({
+        type: 'number',
+        pk: function() {
+            var date = $(this).closest(".calendar__day").data('date');
+            var roomType = $(this).closest(".calendar__day__room_data").data('room-type');
+            return (date && roomType) ? {date: date, roomType: roomType} : '';
+        },
+        url: '/update',
+        title: 'Change Price',
+        name: 'roomPrice'
+    });
 });
