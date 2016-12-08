d7d30b84e38700a21e90f7226711c6d3704ec69b
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2016-12-08 08:28:30 +0530

    Refactor calendar view files

--- code/resources/views/hotels/calendar.blade.php
+++ code/resources/views/hotels/calendar.blade.php
@@ -16,10 +16,12 @@
         </nav>
 
         <section class="calendar">
+
             <div class="row">
                 <div class="col-sm-6">
                     <h1>{{ $calendarWidgetData['calendarTitle'] }}</h1>
                 </div>
+
                 <div class="col-sm-6">
                     <div class="btn-toolbar calendar__toolbar" role="toolbar" aria-label="Toolbar with button groups">
                         <div class="btn-group hidden-sm-down" role="group" aria-label="Basic example">
@@ -36,6 +38,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="row calendar__dates_wrapper hidden-sm-down">
                 <div class="calendar__date">Mon</div>
                 <div class="calendar__date">Tue</div>
@@ -45,9 +48,11 @@
                 <div class="calendar__date">Sat</div>
                 <div class="calendar__date">Sun</div>
             </div>
+
             <div class="row calendar__days_wrapper">
+
                 @for ($i = 0; $i < $calendarWidgetData['startEmptyCells']; $i++)
-                    <div class="calendar__day"></div>
+                    <div class="calendar__day calendar__day_empty"></div>
                 @endfor
 
                 @for ($i = 1; $i <= $calendarWidgetData['daysInMonth']; $i++)
@@ -55,22 +60,14 @@
                         <div class="calendar__day__date">{{ $i }}</div>
                         <div>
                             @foreach ($hotelRoomTypesData as $hotelRoomTypeData)
-                                <div class="row calendar__day__room_data bg_{{ $hotelRoomTypeData['room_type_id'] }}" data-room-type="1">
-                                    <div class="col-lg-4">
-                                        <button type="button" class="btn btn-secondary btn-round">
-                                            <i class="icon-bed"></i>
-                                            <span class="tag tag-round-text tag-warning">{{ $hotelRoomTypeData['room_type_id'] }}</span>
-                                        </button>
+                                <div class="calendar__day__data_section left_border_{{ $hotelRoomTypeData['room_type_id'] }}">
+                                    <div>
+                                        <i class="icon-book"></i>
+                                        <a class="calendar__day__room_count" href="#">{{ $hotelRoomTypeData['default_count'] }}</a>
                                     </div>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <i class="icon-book"></i>
-                                            <a class="calendar__day__room_count" href="#">{{ $hotelRoomTypeData['default_count'] }}</a>
-                                        </div>
-                                        <div>
-                                            <i class="icon-shop"></i>
-                                            <a class="calendar__day__room_price" href="#">{{ $hotelRoomTypeData['default_price'] }}</a> IDR
-                                        </div>
+                                    <div>
+                                        <i class="icon-shop"></i>
+                                        <a class="calendar__day__room_price" href="#">{{ $hotelRoomTypeData['default_price'] }}</a> IDR
                                     </div>
                                 </div>
                             @endforeach
@@ -79,114 +76,12 @@
                 @endfor
 
                 @for ($i = 0; $i < $calendarWidgetData['endEmptyCells']; $i++)
-                    <div class="calendar__day"></div>
+                    <div class="calendar__day calendar__day_empty"></div>
                 @endfor
             </div>
         </section>
 
-        <div class="modal fade" id="mod_bulk_operations">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <form name="fm_bulk_operations" method="post" href="$hotelUrl">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            <h4 class="modal-title">Bulk Operations</h4>
-                        </div>
-                        <div class="modal-body">
-                            <div class="form-group">
-                                <label for="hotelRoomType">Select Room</label>
-                                <select class="form-control" name="hotelRoomType">
-                                    <option value="1">Single Room</option>
-                                    <option value="1">Double Room</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-xs-6">
-                                        <label for="startDate">Start Date</label>
-                                        <input type="date" name="startDate" min="{{ date('Y-m-d') }}">
-                                    </div>
-                                    <div class="col-xs-6">
-                                        <label for="endDate">End Date</label>
-                                        <input type="date" name="endDate" min="{{ date('Y-m-d') }}">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        <input type="radio" name="days" value="all_days" checked> All Days
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <input type="radio" name="days" value="all_weekdays"> All Weekdays
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <input type="radio" name="days" value="all_weekends"> All Weekends
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <input type="radio" name="days" value="custom"> Custom
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-xs-3">
-                                            <input type="checkbox" name="days" value="mondays"> Mondays
-                                        </div>
-                                        <div class="col-xs-3">
-                                            <input type="checkbox" name="days" value="tuesdays"> Tuesdays
-                                        </div>
-                                        <div class="col-xs-3">
-                                            <input type="checkbox" name="days" value="wednesdays"> Wednesdays
-                                        </div>
-                                        <div class="col-xs-3">
-                                            <input type="checkbox" name="days" value="thursdays"> Thursdays
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-xs-3">
-                                            <input type="checkbox" name="days" value="fridays"> Fridays
-                                        </div>
-                                        <div class="col-xs-3">
-                                            <input type="checkbox" name="days" value="saturdays"> Saturdays
-                                        </div>
-                                        <div class="col-xs-3">
-                                            <input type="checkbox" name="days" value="sundays"> Sundays
-                                        </div>
-                                        <div class="col-xs-3">
-                                            &nbsp;
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-xs-6">
-                                        <label for="defaultCount">Availability</label>
-                                        <br/>
-                                        <input type="number" name="defaultCount">
-                                    </div>
-                                    <div class="col-xs-6">
-                                        <label for="defaultPrice">Price</label>
-                                        <br/>
-                                        <input type="number" name="defaultPrice">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+        @include('hotels.calendar_bulk_insertion_form')
 
     </div>
 @endsection
---
+++ code/resources/views/hotels/calendar_bulk_insertion_form.blade.php
@@ -0,0 +1,103 @@
+<div class="modal fade" id="mod_bulk_operations">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form name="fm_bulk_operations" method="post" href="$hotelUrl">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">Bulk Operations</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="hotelRoomType">Select Room</label>
+                        <select class="form-control" name="hotelRoomType">
+                            <option value="1">Single Room</option>
+                            <option value="1">Double Room</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <label for="startDate">Start Date</label>
+                                <input type="date" name="startDate" min="{{ date('Y-m-d') }}">
+                            </div>
+                            <div class="col-xs-6">
+                                <label for="endDate">End Date</label>
+                                <input type="date" name="endDate" min="{{ date('Y-m-d') }}">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <input type="radio" name="days" value="all_days" checked> All Days
+                            </div>
+                            <div class="col-xs-3">
+                                <input type="radio" name="days" value="all_weekdays"> All Weekdays
+                            </div>
+                            <div class="col-xs-3">
+                                <input type="radio" name="days" value="all_weekends"> All Weekends
+                            </div>
+                            <div class="col-xs-3">
+                                <input type="radio" name="days" value="custom"> Custom
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <input type="checkbox" name="days" value="mondays"> Mondays
+                                </div>
+                                <div class="col-xs-3">
+                                    <input type="checkbox" name="days" value="tuesdays"> Tuesdays
+                                </div>
+                                <div class="col-xs-3">
+                                    <input type="checkbox" name="days" value="wednesdays"> Wednesdays
+                                </div>
+                                <div class="col-xs-3">
+                                    <input type="checkbox" name="days" value="thursdays"> Thursdays
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <input type="checkbox" name="days" value="fridays"> Fridays
+                                </div>
+                                <div class="col-xs-3">
+                                    <input type="checkbox" name="days" value="saturdays"> Saturdays
+                                </div>
+                                <div class="col-xs-3">
+                                    <input type="checkbox" name="days" value="sundays"> Sundays
+                                </div>
+                                <div class="col-xs-3">
+                                    &nbsp;
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <label for="defaultCount">Availability</label>
+                                <br/>
+                                <input type="number" name="defaultCount">
+                            </div>
+                            <div class="col-xs-6">
+                                <label for="defaultPrice">Price</label>
+                                <br/>
+                                <input type="number" name="defaultPrice">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
