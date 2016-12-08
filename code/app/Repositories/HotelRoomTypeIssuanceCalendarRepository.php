3a19fdb905eaf7b2a7d8cd63d33b76df98664708
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2016-12-08 18:31:24 +0530

    Adding HotelRoomTypeIssuanceCalendar model functions; Initial commit

--- code/app/Repositories/HotelRoomTypeIssuanceCalendarRepository.php
+++ code/app/Repositories/HotelRoomTypeIssuanceCalendarRepository.php
@@ -12,4 +12,27 @@ class HotelRoomTypeIssuanceCalendarRepository
     {
         $this->hotelRoomTypeIssuanceCalendar = $hotelRoomTypeIssuanceCalendar;
     }
+
+    public function getCalendarDataByMonth($month, $hotelId)
+    {
+        if ($month && $hotelId) {
+            list($yearNo, $monthNo) = explode('-', $month);
+            $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $monthNo, $yearNo);
+
+            return $this->hotelRoomTypeIssuanceCalendar
+                ->join('hotels_room_types', 'hotels_room_types_issuance_calendar.hotels_room_type_id', '=', 'hotels_room_types.id')
+                ->where([
+                ['hotels_room_types.hotel_id', '=', $hotelId],
+                ['date', '>=', $month . '-01'],
+                ['date', '<=', $month . '-' . $daysInMonth]
+            ])->get();
+        }
+    }
+
+    function insertOrUpdateIfExists($data)
+    {
+        $sql = $this->hotelRoomTypeIssuanceCalendar->getInsertOrUpdateIfExistsQuery($data);
+        $this->hotelRoomTypeIssuanceCalendar->getConnection()->statement($sql);
+
+    }
 }
