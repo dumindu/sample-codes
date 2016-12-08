d86aff7dd45ddcdb5ae1a712ea2343fa382b1ffe
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2016-12-08 19:54:37 +0530

    Refactor getCalendarDataByMonth method

--- code/app/Repositories/HotelRoomTypeIssuanceCalendarRepository.php
+++ code/app/Repositories/HotelRoomTypeIssuanceCalendarRepository.php
@@ -15,16 +15,35 @@ class HotelRoomTypeIssuanceCalendarRepository
 
     public function getCalendarDataByMonth($month, $hotelId)
     {
+        $rawData = $this->getRawCalendarDataByMonth($month, $hotelId);
+
+        $result = [];
+        if ($rawData) {
+            foreach ($rawData as $row) {
+                $result[$row['date']] = isset($result[$row['date']]) ? $result[$row['date']] : [];
+                $result[$row['date']][$row['hotels_room_type_id']] = [
+                    'defaultCount' => $row['default_count'],
+                    'defaultPrice' => $row['default_price']
+                ];
+            }
+        }
+
+        return $result;
+    }
+
+    protected function getRawCalendarDataByMonth($month, $hotelId)
+    {
         if ($month && $hotelId) {
             list($yearNo, $monthNo) = explode('-', $month);
             $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $monthNo, $yearNo);
 
             return $this->hotelRoomTypeIssuanceCalendar
+                ->select('hotels_room_types_issuance_calendar.*')
                 ->join('hotels_room_types', 'hotels_room_types_issuance_calendar.hotels_room_type_id', '=', 'hotels_room_types.id')
                 ->where([
-                ['hotels_room_types.hotel_id', '=', $hotelId],
-                ['date', '>=', $month . '-01'],
-                ['date', '<=', $month . '-' . $daysInMonth]
+                    ['hotels_room_types.hotel_id', '=', $hotelId],
+                    ['date', '>=', $month . '-01'],
+                    ['date', '<=', $month . '-' . $daysInMonth]
             ])->get();
         }
     }
