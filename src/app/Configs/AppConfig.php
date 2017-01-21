0e73e485155473f4234893ef9407722abcf12fba
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2017-01-21 20:10:28 +0530

    Adding illuminate/database as an ORM layer

--- src/app/Configs/AppConfig.php
+++ src/app/Configs/AppConfig.php
@@ -7,6 +7,16 @@ class AppConfig {
     const ERROR_REPORTING = 'E_ALL';
     const DISPLAY_ERRORS = 1;
 
+    const DB_CONFIGS = [
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'php-skeleton',
+        'username'  => 'php-skeleton',
+        'password'  => 'php-skeleton',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ];
+
     const STATIC_PATHS = [
         'viewDir' => __DIR__ . '/../../../resources/views'
     ];
